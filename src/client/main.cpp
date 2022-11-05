213674f77556286959d6017221ff687ab7a45eb6
lucas-mrq <lucasmarquet63800@gmail.com>
2022-11-05 16:26:14 +0100

    Open first sfml window

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <client.hpp>
+#include <SFML/Graphics.hpp>
 
 int main(int argc,char* argv[])
 {
+    
     std::string arg;
     if (argc == 2) client::sayHello sayHello(argv[1]);
 
@@ -10,6 +12,23 @@ int main(int argc,char* argv[])
         std::cout << "Unkown command, use -h for help." << std::endl;
     }
 
-   
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
