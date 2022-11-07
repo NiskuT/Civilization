1571619fd055361710d2d16ec2ae955f66b1f09d
lucas-mrq <lucasmarquet63800@gmail.com>
2022-11-07 14:11:17 +0100

    Display a first map

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,7 +2,108 @@
 #include <client.hpp>
 #include <SFML/Graphics.hpp>
 
-int main(int argc,char* argv[])
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+
+    bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+    {
+        // load the tileset texture
+        if (!m_tileset.loadFromFile(tileset))
+            return false;
+
+        // resize the vertex array to fit the level size
+        m_vertices.setPrimitiveType(sf::Quads);
+        m_vertices.resize(width * height * 4);
+
+        // populate the vertex array, with one quad per tile
+        for (unsigned int i = 0; i < width; ++i)
+            for (unsigned int j = 0; j < height; ++j)
+            {
+                // get the current tile number
+                int tileNumber = tiles[i + j * width];
+
+                // find its position in the tileset texture
+                int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+                int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+
+                // get a pointer to the current tile's quad
+                sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+
+                // define its 4 corners
+                quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+                quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+                quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+                quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+
+                // define its 4 texture coordinates
+                quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+                quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+                quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+                quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+            }
+
+        return true;
+    }
+
+private:
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        // apply the transform
+        states.transform *= getTransform();
+
+        // apply the tileset texture
+        states.texture = &m_tileset;
+
+        // draw the vertex array
+        target.draw(m_vertices, states);
+    }
+
+    sf::VertexArray m_vertices;
+    sf::Texture m_tileset;
+};
+
+class Hex {
+
+    public:
+
+        sf::Sprite sprite;
+
+        void setTexture(sf::Texture texture) 
+        {
+            sprite.setTexture(texture);
+            sprite.setPosition({50.f,0.f});
+        }
+};
+
+class Map {
+
+    public:
+
+        Hex water;     
+
+        Map()
+        {
+            sf::Texture water_texture;
+            if (!water_texture.loadFromFile("../ressources/img/map/field-water.png"))
+            {
+                std::cout << "Error loading water_texture";
+            }
+
+            water.setTexture(water_texture);
+            
+        };
+
+        void displayMap (sf::RenderWindow window) 
+        {
+            window.draw(water.sprite);
+        }
+
+};
+
+
+/*int main(int argc,char* argv[])
 {
     
     std::string arg;
@@ -12,10 +113,10 @@ int main(int argc,char* argv[])
         std::cout << "Unkown command, use -h for help." << std::endl;
     }
 
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1200, 800), "Civilization VII");
 
+    Map map;
+    
     while (window.isOpen())
     {
         sf::Event event;
@@ -26,7 +127,50 @@ int main(int argc,char* argv[])
         }
 
         window.clear();
-        window.draw(shape);
+        map.displayMap(window);
+        window.display();
+    }
+
+    return 0;
+}*/
+
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(512, 256), "Tilemap");
+
+    // define the level with an array of tile indices
+    const int level[] =
+    {
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+    };
+
+    // create the tilemap from the level definition
+    TileMap map;
+    if (!map.load("../ressources/tileset.png", sf::Vector2u(32, 32), level, 16, 8))
+        return -1;
+
+    // run the main loop
+    while (window.isOpen())
+    {
+        // handle events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // draw the map
+        window.clear();
+        window.draw(map);
         window.display();
     }
 
