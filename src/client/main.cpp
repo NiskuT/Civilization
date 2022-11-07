43e4ef4352c8943f094ceb4e51d78d0b40b4edfa
lucas-mrq <lucasmarquet63800@gmail.com>
2022-11-07 15:11:44 +0100

    Change the map to the exemple map

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,7 +2,9 @@
 #include <client.hpp>
 #include <SFML/Graphics.hpp>
 
-class TileMap : public sf::Drawable, public sf::Transformable
+#define Y_OFFSET -21
+
+class FieldMap : public sf::Drawable, public sf::Transformable
 {
 public:
 
@@ -16,31 +18,39 @@ public:
         m_vertices.setPrimitiveType(sf::Quads);
         m_vertices.resize(width * height * 4);
 
+        unsigned int offset = 0;
+
         // populate the vertex array, with one quad per tile
         for (unsigned int i = 0; i < width; ++i)
             for (unsigned int j = 0; j < height; ++j)
             {
-                // get the current tile number
-                int tileNumber = tiles[i + j * width];
+                if (j%2==0) offset = 41;
+                else offset = 0;
 
-                // find its position in the tileset texture
-                int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
-                int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+                if ((offset == 0) | (i+1 != width))
+                {
+                    // get the current tile number
+                    int tileNumber = tiles[i + j * width];
 
-                // get a pointer to the current tile's quad
-                sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+                    // find its position in the tileset texture
+                    int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+                    int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
 
-                // define its 4 corners
-                quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
-                quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
-                quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
-                quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+                    // get a pointer to the current tile's quad
+                    sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
 
-                // define its 4 texture coordinates
-                quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
-                quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
-                quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
-                quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+                    // define its 4 corners
+                    quad[0].position = sf::Vector2f(offset + i * tileSize.x, j * (Y_OFFSET + tileSize.y));
+                    quad[1].position = sf::Vector2f(offset + (i + 1) * tileSize.x, j * (Y_OFFSET + tileSize.y));
+                    quad[2].position = sf::Vector2f(offset + (i + 1) * tileSize.x, j * (Y_OFFSET + tileSize.y) + tileSize.y);
+                    quad[3].position = sf::Vector2f(offset + i * tileSize.x, j * (Y_OFFSET + tileSize.y) + tileSize.y);
+
+                    // define its 4 texture coordinates
+                    quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+                    quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+                    quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+                    quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+                }
             }
 
         return true;
@@ -64,46 +74,7 @@ private:
     sf::Texture m_tileset;
 };
 
-class Hex {
-
-    public:
-
-        sf::Sprite sprite;
-
-        void setTexture(sf::Texture texture) 
-        {
-            sprite.setTexture(texture);
-            sprite.setPosition({50.f,0.f});
-        }
-};
-
-class Map {
-
-    public:
-
-        Hex water;     
-
-        Map()
-        {
-            sf::Texture water_texture;
-            if (!water_texture.loadFromFile("../ressources/img/map/field-water.png"))
-            {
-                std::cout << "Error loading water_texture";
-            }
-
-            water.setTexture(water_texture);
-            
-        };
-
-        void displayMap (sf::RenderWindow window) 
-        {
-            window.draw(water.sprite);
-        }
-
-};
-
-
-/*int main(int argc,char* argv[])
+int main(int argc,char* argv[])
 {
     
     std::string arg;
@@ -113,51 +84,30 @@ class Map {
         std::cout << "Unkown command, use -h for help." << std::endl;
     }
 
-    sf::RenderWindow window(sf::VideoMode(1200, 800), "Civilization VII");
-
-    Map map;
-    
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        map.displayMap(window);
-        window.display();
-    }
-
-    return 0;
-}*/
-
-int main()
-{
     // create the window
-    sf::RenderWindow window(sf::VideoMode(512, 256), "Tilemap");
+    sf::RenderWindow window(sf::VideoMode(1245, 725), "Civilization VII");
 
     // define the level with an array of tile indices
     const int level[] =
     {
-        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
-        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
-        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
-        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
-        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
-        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
-        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+        0, 0, 5, 4, 4, 2, 5, 2, 1, 2, 5, 0, 0, 0, 0,
+        0, 0, 0, 5, 0, 1, 5, 5, 5, 5, 1, 1, 0, 0, 0,
+        0, 0, 0, 3, 0, 1, 5, 0, 1, 1, 3, 4, 0, 0, 0,
+        0, 0, 0, 0, 1, 2, 5, 3, 2, 3, 1, 4, 0, 0, 0,
+        0, 0, 0, 0, 4, 5, 5, 2, 3, 0, 3, 2, 0, 0, 0,
+        0, 0, 0, 0, 2, 5, 1, 3, 3, 2, 2, 5, 0, 0, 0,
+        0, 0, 5, 1, 2, 1, 5, 3, 1, 5, 2, 5, 4, 0, 0,
+        0, 0, 0, 1, 2, 0, 4, 2, 5, 3, 1, 4, 5, 0, 0,
+        0, 0, 0, 0, 2, 0, 5, 4, 2, 0, 0, 3, 2, 2, 0,
+        0, 0, 0, 0, 1, 1, 5, 3, 0, 5, 2, 4, 2, 2, 0,
+        0, 0, 0, 0, 0, 1, 5, 5, 0, 1, 0, 0, 4, 2, 0,
     };
 
     // create the tilemap from the level definition
-    TileMap map;
-    if (!map.load("../ressources/tileset.png", sf::Vector2u(32, 32), level, 16, 8))
+    FieldMap fieldMap;
+    if (!fieldMap.load("../ressources/img/map/field.png", sf::Vector2u(83, 85), level, 15, 11))
         return -1;
-
-    // run the main loop
+    
     while (window.isOpen())
     {
         // handle events
@@ -170,7 +120,7 @@ int main()
 
         // draw the map
         window.clear();
-        window.draw(map);
+        window.draw(fieldMap);
         window.display();
     }
 
