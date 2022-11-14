6bf105b6a93d9118d4f931976fc28e37111ddc98
Vio1625 <violaine.bec@gmail.com>
2022-11-14 17:07:07 +0100

    [feature-13] Create HudDisplay class to display background, ladder, tech and barbarian wheel

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,8 +2,79 @@
 #include <client.hpp>
 #include <SFML/Graphics.hpp>
 
+
+class HudDisplay {
+    private:
+        sf::Sprite sprite;
+        sf::Texture texture;
+        std::string path;
+        sf::Vector2<unsigned int> size;
+        float scale;
+        std::string type;
+
+    public:
+        HudDisplay(std::string path, float scale, std::string type ) {
+            this->sprite = sprite;
+            this->texture = texture;
+            this->path = path;
+            this->scale = scale;
+            this->type = type;
+
+            if (!texture.loadFromFile(path)){
+                std::cout << "Error loading background";
+            }
+            
+        sprite.setScale(scale, scale);
+        sprite.setTexture(texture);
+        
+        }
+
+        sf::Sprite getSprite() {return sprite;};
+
+        void updatePlacement(int length, int width, int priorityCard){
+        sf::Vector2<unsigned int> size = this->texture.getSize();
+        int offsetLength = 0;
+        int offsetWidth = 0;
+        int rotation = 0; 
+            if (this->type == "background") {
+                
+                offsetLength = -((size.x-length)/2);
+                offsetWidth = -((size.y-width)/2);
+            }
+
+            else if (this->type == "ladder") {
+                offsetLength = (length-size.x)/2;
+                offsetWidth = (width-size.y);  
+            }
+
+            else if (this->type =="techWheel") {
+                rotation = 90;
+                this->sprite.setOrigin(size.x/2, size.y/2); // set the point of rotation at the center of the image
+                offsetLength = length;
+                offsetWidth = width; 
+            }
+
+            else if (this->type == "barbareWheel") {
+                offsetWidth = (width-size.y*0.3);
+            }
+
+            else if (this->type == "priorityCard") {
+                offsetLength = 10*priorityCard + 300;
+                offsetWidth = width-size.y; 
+            }
+
+            else {
+                std::cout << ("Not good type");
+            }
+            sprite.rotate(rotation);
+            sprite.move(offsetLength, offsetWidth);
+            
+        }
+};
+
 int main(int argc,char* argv[])
 {
+
     
     std::string arg;
     if (argc == 2) client::sayHello sayHello(argv[1]);
@@ -17,67 +88,48 @@ int main(int argc,char* argv[])
     int width = 900;
     sf::RenderWindow window(sf::VideoMode(length, width), "Civilization VII");
 
+
     // Display the background
-    sf::Sprite background;
-    sf::Texture backgroundTexture;
-    if (!backgroundTexture.loadFromFile("../ressources/img/hud/background.png"))
-    {
-        std::cout << "Error loading background";
-    }
-    background.setTexture(backgroundTexture);
-    sf::Vector2<unsigned int> sizeBackground = backgroundTexture.getSize();
-    int offsetBackground = (sizeBackground.y-width)/2; // move the background in width so that it is at the middle of the window
-    background.move(0,-offsetBackground);
+    HudDisplay background("../ressources/img/hud/background.png", 1, "background");
+    background.updatePlacement(length, width, 0);
 
     // Display ladder
 
-    sf::Sprite ladder;
-    sf::Texture ladderTexture;
-    if (!ladderTexture.loadFromFile("../ressources/img/hud/ladder.png"))
-    {
-        std::cout << "Error loading ladder";
-    }
-    ladder.setTexture(ladderTexture);
-    sf::Vector2<unsigned int> sizeLadder = ladderTexture.getSize();
-    int offsetWidthLadder = (width-sizeLadder.y);
-    int offsetLengthLadder = (length-sizeLadder.x)/2;
-    ladder.move(offsetLengthLadder,offsetWidthLadder);
+    HudDisplay ladder("../ressources/img/hud/ladder.png",1 , "ladder");
+    ladder.updatePlacement(length, width, 0);
+    
 
     //Display tech wheel
-    sf::Sprite techWheel;
-    sf::Texture techWheelTexture;
-    int rotation = 45;
-    if (!techWheelTexture.loadFromFile("../ressources/img/hud/tech-wheel.png"))
-    {
-        std::cout << "Error loading tech wheele";
-    }
-    techWheel.setScale(0.3f, 0.3f);
-    techWheel.setTexture(techWheelTexture);
-    sf::Vector2<unsigned int> sizeTechWheel = techWheelTexture.getSize();
-    techWheel.setOrigin(sizeTechWheel.x/2, sizeTechWheel.y/2); // set the point of rotation at the center of the image
-    techWheel.move(length, width); // set the point of rotation at the bottom right
-    techWheel.rotate(rotation);
+
+    HudDisplay techWheel("../ressources/img/hud/tech-wheel.png", 0.3f, "techWheel");
+    techWheel.updatePlacement(length,width, 0);
 
     // Display event dial (barbare wheel)
-    int posBarbareWheel = 0;
 
-    sf::Sprite barbareWheel;
-    sf::Texture barbareWheelTexture;
+    int posBarbareWheel = 0;
     std::string file = "../ressources/img/hud/barbare-wheel-";
     std::string num = std::to_string(posBarbareWheel);
     std::string format= ".png";
     std::string fileToLoad = file + num + format;
-    if (!barbareWheelTexture.loadFromFile(fileToLoad))
-    {
-        std::cout << "Error loading barbare wheel";
+    HudDisplay barbareWheel(fileToLoad, 0.3f, "barbareWheel");
+    barbareWheel.updatePlacement(length, width, 0);
+
+
+    // Display card
+
+/*
+    std::vector<std::string> cardType = {"army", "culture", "economy", "industry", "science"};
+    std::vector<HudDisplay> priorityCards;
+    for (int i=0; i<1; i++) {
+        std::string file = "../ressources/img/hud/priority-card-";
+        std::string type = cardType[i];
+        std::string format= ".png";
+        std::string fileToLoad = file + type + format;
+        priorityCards.emplace_back(fileToLoad, 1, "priorityCard") ;
+        priorityCards.back().updatePlacement(length, width, i);
     }
-    barbareWheel.setScale(0.3f, 0.3f);
-    barbareWheel.setTexture(barbareWheelTexture);
-    sf::Vector2<unsigned int> sizeBarbareWheel = barbareWheelTexture.getSize();
-    int offsetWidthBarbareWheel = (width-sizeBarbareWheel.y*0.3);
-    barbareWheel.move(0, offsetWidthBarbareWheel);
-
-
+*/
+   
 
 
 
@@ -110,10 +162,10 @@ int main(int argc,char* argv[])
 
         // draw the map
         window.clear();
-        window.draw(background);
-        window.draw(ladder);
-        window.draw(techWheel);
-        window.draw(barbareWheel);
+        window.draw(background.getSprite());
+        window.draw(ladder.getSprite());
+        window.draw(techWheel.getSprite());
+        window.draw(barbareWheel.getSprite());
         //window.draw(map);
         window.display();
     }
