02f2d09d99e1b9309f1781262ec3d2c3a4caf011
lucas-mrq <lucasmarquet63800@gmail.com>
2023-01-25 15:53:38 +0100

    improvement-27 Add a test for ControlPawn

---
+++ test/shared/test_controlpawn.cpp
@@ -0,0 +1,32 @@
+#include <shared.hpp>
+#include <ctime>
+#include <iostream>
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE( ControlPawnTest )
+
+BOOST_AUTO_TEST_CASE(TestStaticAssert)
+{
+  BOOST_CHECK(1);
+}
+
+BOOST_AUTO_TEST_CASE( constructorAndGetters )
+{
+    shared::ControlPawn pawn({20, 15});
+
+    BOOST_CHECK(pawn.getPosition()[0] == 20);
+    BOOST_CHECK(pawn.getPosition()[1] == 15);
+    BOOST_CHECK(pawn.isReinforced() == false);
+
+    shared::ElementEnum type = pawn.getType();
+    BOOST_CHECK(type == shared::ElementEnum::controlPawn);
+}
+
+BOOST_AUTO_TEST_CASE( setReinforced )
+{
+    shared::ControlPawn pawn({0, 0});
+    pawn.setReinforced();
+    BOOST_CHECK(pawn.isReinforced() == true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
