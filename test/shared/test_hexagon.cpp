605d12fe84494aecbda31d8948d6aacc88dc998a
lucas-mrq <lucasmarquet63800@gmail.com>
2023-01-25 15:48:45 +0100

    improvement-27 Add a test for Hexagon

---
+++ test/shared/test_hexagon.cpp
@@ -0,0 +1,56 @@
+#include <shared.hpp>
+#include <ctime>
+#include <iostream>
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE( HexagonTest )
+
+BOOST_AUTO_TEST_CASE(TestStaticAssert)
+{
+  BOOST_CHECK(1);
+}
+
+BOOST_AUTO_TEST_CASE( constructorAndGetters )
+{
+    shared::Hexagon hexagon;
+
+    BOOST_CHECK(hexagon.getFieldLevel() == shared::FieldLevel::Grassland);
+}
+
+BOOST_AUTO_TEST_CASE( setFieldType )
+{
+    shared::Hexagon hexagon;
+
+    hexagon.setFieldType(shared::FieldLevel::WonderMessa);
+
+    BOOST_CHECK(hexagon.getFieldLevel() == shared::FieldLevel::WonderMessa);
+}
+
+BOOST_AUTO_TEST_CASE( manageElements )
+{
+    shared::Hexagon hexagon;
+
+    BOOST_CHECK(hexagon.getElements().size() == 0);
+
+    std::shared_ptr<shared::Caravan> caravan1 = std::make_shared<shared::Caravan>();
+    std::variant<shared::Caravan, shared::Barbarian, shared::BarbarianVillage, shared::ControlPawn, shared::City> element1 = *caravan1;
+    hexagon.addElement(std::make_shared<std::variant<shared::Caravan, shared::Barbarian, shared::BarbarianVillage, shared::ControlPawn, shared::City>>(element1));
+
+    BOOST_CHECK(hexagon.getElements().size() == 1);
+
+    std::shared_ptr<shared::Caravan> caravan2 = std::make_shared<shared::Caravan>();
+    std::variant<shared::Caravan, shared::Barbarian, shared::BarbarianVillage, shared::ControlPawn, shared::City> element2 = *caravan2;
+    hexagon.addElement(std::make_shared<std::variant<shared::Caravan, shared::Barbarian, shared::BarbarianVillage, shared::ControlPawn, shared::City>>(element2));
+
+    BOOST_CHECK(hexagon.getElements().size() == 2);
+
+    hexagon.removeElement(hexagon.getElements()[0]);
+
+    BOOST_CHECK(hexagon.getElements().size() == 1);
+
+    hexagon.clearElement();
+
+    BOOST_CHECK(hexagon.getElements().size() == 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
